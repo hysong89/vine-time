195557c2c60e90326faba9c6c95b2db74bee16ff
Mike Chau <mchau@iwu.edu>
2013-07-29 04:51:34 -0500

    Add perm 301 redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :ensure_domain
+
+  APP_DOMAIN = 'www.vynetime.com'
+
+  def ensure_domain
+    if request.env['HTTP_HOST'] != APP_DOMAIN && ENV["RAILS_ENV"] != 'development' && ENV["RAILS_ENV"] != 'test'
+      # HTTP 301 is a "permanent" redirect
+      redirect_to "http://#{APP_DOMAIN}", :status => 301
+    end
+  end
 end
